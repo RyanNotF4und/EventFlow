bc59e63deba3e71b8632ee19624f4051f760b594
RyanNotF4und <ryan.nascimento230@gmail.com>
2023-03-09 21:11:04 -0300

    nao mostrar evento depois de expirado

--- php/display.inc.php
+++ php/display.inc.php
@@ -4,7 +4,7 @@ class Display extends db
 {
     public function caroussel()
     {
-        $result = $this->connect()->query("SELECT * FROM events ORDER BY views DESC LIMIT 0,3");
+        $result = $this->connect()->query("SELECT * FROM events WHERE date_event >= NOW() ORDER BY views DESC LIMIT 0,3");
         if($result->num_rows > 0) {
             return $result;
         } else {
@@ -14,7 +14,7 @@ class Display extends db
 
     public function highLights()
     {
-        $result = $this->connect()->query("SELECT * FROM events ORDER BY views DESC LIMIT 3,5");
+        $result = $this->connect()->query("SELECT * FROM events WHERE date_event >= NOW() ORDER BY views DESC LIMIT 3,5");
         if($result->num_rows > 0) {
             return $result;
         } else {
