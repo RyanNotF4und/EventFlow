87af7acfaec67cc419a4ece262878cca882daa48
RyanNotF4und <ryan.nascimento230@gmail.com>
2023-03-17 20:42:14 -0300

    lojinha

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,39 +10,5 @@ $(function () {
     $('#search').click(function () {
         $('#search-input').slideToggle();
     })
-
-
-
-
-
-
-    
-    $('#mobile > #options').click(function () {
-        $('header > div > #mobile > ul').slideToggle();
-    })
-    $('#mobile > #search').click(function () {
-        $('header > #form_mobile > input').slideToggle();
-        $('#mobile > #search').css('display', 'none');
-        $('#close').slideToggle();
-    })
-    $('#user').click(function () {
-        $('#userUl').slideToggle();
-    })
-    //
-    $('#close').click(function () {
-        $('header > #form_mobile > input').slideToggle();
-        $('#close').slideToggle();
-        $('#mobile > #search').fadeIn();
-    })
-    $(window).resize(function () {
-        if ($(window).width() >= 610) {
-            $('#mobile_search_bar').css("display", "none");
-            $('#close').css("display", "none");
-        } else {
-            $('#mobile_search_bar').css("display", "block");
-            $('#close').css("display", "block");
-            $('#search').css("display", "none");
-        }
-    });
 })
 
